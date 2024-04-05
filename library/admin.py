6ec3c8bfc406eeb7a10e343f160cfef3d12b791b
mirjaxon17 <138755405+mirjaxon17@users.noreply.github.com>
2024-04-05 17:29:53 +0300

    Update admin.py

--- library/admin.py
+++ library/admin.py
@@ -1,7 +1,16 @@
 from django.contrib import admin
-from .models import Book,Autor,BookingBook,Comment
+from .models import Student,Adress
+# Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Autor)
-admin.site.register(BookingBook)
-admin.site.register(Comment)
+@admin.register(Adress)
+class AdressAdmin(admin.ModelAdmin):
+    list_display = ("id",'name',)
+    list_display_links = ("id",'name',)
+    search_fields =("name",)
+
+@admin.register(Student)
+class StudentAdmin(admin.ModelAdmin):
+    list_display = ("id",'first_name','last_name','status',"age","phone_number")
+    list_display_links = ('id','first_name','last_name','status',"age","phone_number")
+    search_fields = ('first_name','age','id')
+    ordering = ('id',)
